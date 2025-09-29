5c9a4c34c850c0a172dcbf1090632f33233141ca
amurphy99 <amurphy62299@gmail.com>
2025-09-29 14:35:10 -0400

    end chat integrated officially

--- app/src/main/java/com/example/buddychat/stt/STTCallbacks.java
+++ app/src/main/java/com/example/buddychat/stt/STTCallbacks.java
@@ -14,6 +14,9 @@ import com.example.buddychat.utils.audio_triangulation.AudioTracking;
 import com.example.buddychat.utils.audio_triangulation.AngleBuckets;
 import com.example.buddychat.utils.audio_triangulation.AngleBuckets.Bucket;
 
+import com.example.buddychat.chat.ChatStatus;
+import com.example.buddychat.chat.ChatController;
+
 // =======================================================================
 // Handles Recognized Speech Events
 // =======================================================================
@@ -42,19 +45,20 @@ public class STTCallbacks implements STTListener {
     @SuppressLint("DefaultLocale")
     @Override public void onText(String utterance, float confidence, String rule) {
         // Operations that need to happen on detection of a user utterance
-        final String logMsg = onUserUtterance(utterance);
+        final boolean chatEnded   = onUserUtterance(utterance);
+        final boolean awakeStatus = ChatStatus.isRunning();
 
         // Do some stuff on the UI thread
         ui.post(() -> {
             // Send the message over the WebSocket
             utteranceCallback.sendString(utterance);
 
+            // ToDo: IDK how this interaction is going to work at all
+            if (chatEnded && awakeStatus) { ChatController.end(); }
+
             // Logging the message
             Log.i(TAG, String.format("%s Utt: %s (conf: %.3f, rule: %s)", TAG, utterance, confidence, rule));
             sttView  .setText(String.format("User (%.3f): %s", (confidence/1_000), utterance));
-
-            // ToDo: Update the debug text view
-            testView1.setText(logMsg);
         });
     }
 
@@ -65,7 +69,7 @@ public class STTCallbacks implements STTListener {
     // Other Helpers
     // -----------------------------------------------------------------------
     /** Method that runs when a user's utterance is detected. */
-    private String onUserUtterance(String utterance) {
+    private boolean onUserUtterance(String utterance) {
         // Set Buddy's face to "THINKING" while we wait for the backend
         Emotions.setMood(FacialExpression.THINKING);
 
@@ -81,7 +85,12 @@ public class STTCallbacks implements STTListener {
 
         // Log and return -- ToDo: Normally we would combine the audio string with the user intent string...
         Log.d(TAG, String.format("%s onUserUtt: %s", TAG, userLog));
-        return userLog;
+
+
+        // ToDo: Update the debug text view
+        // ui.post( () -> { testView1.setText(logMsg); });
+
+        return userEndChat;
     }
 
 
