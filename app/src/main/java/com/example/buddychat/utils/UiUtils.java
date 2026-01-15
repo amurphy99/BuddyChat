276d7c91f3c9f4835ad69c1e201666f966a652ae
amurphy99 <amurphy62299@gmail.com>
2026-01-15 14:44:51 -0600

    added some text setters for main UI changes

--- app/src/main/java/com/example/buddychat/utils/UiUtils.java
+++ app/src/main/java/com/example/buddychat/utils/UiUtils.java
@@ -2,6 +2,7 @@ package com.example.buddychat.utils;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.buddychat.MainApplication;
@@ -12,6 +13,9 @@ import com.example.buddychat.MainApplication;
 public class UiUtils {
     private static final String TAG  = "[DPU_UiUtils]";
 
+    // ToDo: Might want to make a helper to set UI element text? Like the hidden userInfo one or the start/end button
+    // textUserInfo.setText(String.format("%s %s | %s", p.plwd.first_name, p.plwd.last_name, p.plwd.username));
+
     // Helper to show short toasts from ANY thread safely
     public static void showToast(final String message) {
         // This runs on the Main UI thread
@@ -19,6 +23,25 @@ public class UiUtils {
             @Override public void run() {
                 Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_SHORT).show();
             }
+        });
+    }
+
+    // --------------------------------------------------------------------------------
+    // Safe Text Setters -- Safely update a TextView from any thread
+    // --------------------------------------------------------------------------------
+    /// Use when there is a String variable.
+    public static void updateText(final TextView view, final String text) {
+        if (view == null) return; // Safety check
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override public void run() { view.setText(text); }
+        });
+    }
+
+    /// Use when there is a Resource ID (e.g., R.string.welcome_message).
+    public static void updateText(final TextView view, final int stringResId) {
+        if (view == null) return; // Safety check
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override public void run() { view.setText(stringResId); }
         });
     }
 
