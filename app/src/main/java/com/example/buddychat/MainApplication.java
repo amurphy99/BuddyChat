96baa1ac917c1a3643a6680cdae31b7eac5f0d03
amurphy99 <amurphy62299@gmail.com>
2026-01-15 14:19:56 -0600

    save the instance of the main app
    allows us to access the context from anywhere

--- app/src/main/java/com/example/buddychat/MainApplication.java
+++ app/src/main/java/com/example/buddychat/MainApplication.java
@@ -1,9 +1,21 @@
 package com.example.buddychat;
 import com.bfr.buddysdk.BuddyApplication;
 
+import android.content.Context;
+
 public class MainApplication extends BuddyApplication {
-    @Override
-    public void onCreate() {
+
+    // Static variable to hold the context
+    private static MainApplication instance;
+
+    @Override public void onCreate() {
         super.onCreate();
+
+        // Save the instance so it can be accessed later
+        instance = this;
     }
+
+    // Public static method to get the context from anywhere
+    public static Context getAppContext() { return instance.getApplicationContext(); }
+
 }
