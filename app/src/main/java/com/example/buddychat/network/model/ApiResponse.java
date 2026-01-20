646acded2b3f042cd9914fcd27b00a6aeadc9321
amurphy99 <amurphy62299@gmail.com>
2026-01-20 16:31:06 -0600

    unused files

--- app/src/main/java/com/example/buddychat/network/model/ApiResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.buddychat.network.model;
-
-public class ApiResponse {
-    public String status;
-    public String version;
-}
--- app/src/main/java/com/example/buddychat/network/model/AuthListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.buddychat.network.model;
-
-// Allows us to do onSuccess and onFailure stuff
-public interface AuthListener {
-    void onSuccess(String    token);
-    void onError  (Throwable t    );
-}
